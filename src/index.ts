e8cbd6abae4700e356298f7969453f612507bfcf
Andres Felipe Pinto <felpin1945@gmail.com>
2024-07-04 18:50:28 -0500

    cors implemented

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { routerApi } from "./routers";
 import {
   boomErrorHandler,
@@ -8,6 +9,21 @@ import {
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+const whiteList = ["http:localhost:8080", `http:localhost:${port}`];
+const options: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) {
+      callback(null, true);
+    }
+    if (origin && whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed"));
+    }
+  },
+};
+app.use(cors(options));
 
 app.use(express.json());
 
